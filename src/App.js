e62785fdb9e29ea4f90d103239bc9c7dbc1bcf44
yasir-a <yasir.a@zep-com.com>
2022-01-22 21:04:21 +0500

    regEx changed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   const emailValidation = () => {
-    const regEx =
-      /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
+    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z]{2,8})?/g;
     if (regEx.test(email)) {
       setMessage("Email is Valid");
     } else if (!regEx.test(email) && email !== "") {
